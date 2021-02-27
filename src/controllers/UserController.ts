cf169ebf36c66b07d789e75c23ad313c9232f105
Camila Couto <camilacoutodeoliveira@gmail.com>
2021-02-26 21:43:24 -0300

    Projeto NWL04 - NodeJS

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,55 @@
+import { Request, Response } from 'express';
+import { getCustomRepository } from 'typeorm';
+import * as yup from 'yup';
+import { AppError } from '../errors/AppError';
+import { UsersRepository } from '../repositories/UsersRepository';
+
+class UserController {
+
+    async create(request: Request, response: Response) {
+        const { name, email } = request.body;
+        const schema = yup.object().shape({
+            name: yup.string().required(),
+            email: yup.string().email().required(),
+        });
+
+        try {
+            await schema.validate(request.body, { abortEarly: false });
+        } catch (err) {
+            throw new AppError(err);
+        }
+
+        const userRepository = getCustomRepository(UsersRepository);
+
+        const userAlreadyExists = await userRepository.findOne({
+            email
+        });
+        if (userAlreadyExists) {
+            // throw new AppError("User already exists!");
+            return response.status(400).json({
+                error: "User already exists!",
+            });
+        }
+
+        const user = userRepository.create({
+            name,
+            email,
+        });
+
+        await userRepository.save(user);
+
+        return response.status(201).json(user);
+    }
+
+    async show(request: Request, response: Response){
+        const userRepository = getCustomRepository(UsersRepository);
+
+        const all = await userRepository.find();
+
+        return response.json(all);
+     }
+
+}
+
+export { UserController };
+
---
+++ src/database/migrations/1614088107856-CreateUsers.ts
@@ -0,0 +1,38 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateUsers1614088107856 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "users",
+                columns: [
+                    {
+                        name: "Id",
+                        type: "uuid",
+                        isPrimary: true
+                    },
+                    {
+                        name: "name",
+                        type: "varchar",
+
+                    },
+                    {
+                        name: "email",
+                        type: "varchar"
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()"
+                    }
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("users");
+    }
+
+}
